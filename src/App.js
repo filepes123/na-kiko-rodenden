f4d230600081cf0eefe6a13c7dfe4efb8e4ad3ef
filepes123 <65510250+filepes123@users.noreply.github.com>
2022-04-29 13:41:45 +0200

    added music

--- src/App.js
+++ src/App.js
@@ -8,18 +8,19 @@ import Confetti from "react-confetti";
 function App() {
   const { width, height } = useWindowSize();
   const [playing, setPlaying] = useState(false);
-  const audio = useMemo(()=>{
-    const audio1 = new Audio('audio/uck');
+  const [start, setStart] = useState(false);
+  const audio = useMemo(() => {
+    const audio1 = new Audio("audio/uck");
     return audio1;
-  },['audio/uck'])
+  }, []);
 
   useEffect(() => {
-    if(!playing){
+    if (!playing && start) {
       audio.play();
-    }else{
+    } else {
       audio.pause();
     }
-  }, [playing]);
+  }, [playing, start, audio]);
   const handleAudio = (zvuk) => {
     const audio = new Audio(zvuk);
     setPlaying(true);
@@ -31,17 +32,28 @@ function App() {
   return (
     <div className="wrapper">
       <div className="cards">
-        {chestitki.map((data) => (
-          <Card className="card" style={{ width: "18rem" }}>
-            <Card.Img className="card-img" variant="top" src={data.slika} />
-            <Card.Body className="card-body">
-              <Card.Title>{data.ime}</Card.Title>
-              <Button onClick={() => handleAudio(data.audio)} variant="primary">
-                Chestitka
-              </Button>
-            </Card.Body>
-          </Card>
-        ))}
+        {!start ? (
+          <Button onClick={() => setStart(true)} size="lg" variant="primary">
+            STISNI
+          </Button>
+        ) : (
+          <>
+            {chestitki.map((data) => (
+              <Card className="card" style={{ width: "18rem" }}>
+                <Card.Img className="card-img" variant="top" src={data.slika} />
+                <Card.Body className="card-body">
+                  <Card.Title>{data.ime}</Card.Title>
+                  <Button
+                    onClick={() => handleAudio(data.audio)}
+                    variant="primary"
+                  >
+                    Chestitka
+                  </Button>
+                </Card.Body>
+              </Card>
+            ))}
+          </>
+        )}
       </div>
       <Confetti width={width} height={height} numberOfPieces={25} />
     </div>
